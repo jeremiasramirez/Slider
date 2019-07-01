f1b60f25af059a6c413210b227b896e5220eb667
Jeremias ramirez <jereramirez499@gmail.com>
2019-07-01 15:36:20 -0400

    Mejorando el frontend style y agregando un contador + haciendo responsive

--- slider.js
+++ slider.js
@@ -4,27 +4,38 @@ let colors = ["blue","red", "orangered", "magenta", "#333", "purple",
 function goImage(goimg, backimg, listImgs, counter_, sizeListImgs, imgitem, routeimg, mainimg, counterimage){
 // go click
 	goimg.addEventListener("click", (e)=>{
+		 
 	counter_ += 1;
 		counterimage.style.backgroundColor = colors[counter_]
 		if (counter_ < sizeListImgs){
 			imgitem[0].src = routeimg+"/"+listimgs[counter_];
+			goimg.classList.add("scale")
 		}
 		if (counter_ > sizeListImgs-1){
 		counter_ = 0
 		imgitem[0].src = mainimg;
 		}
-  	counterimage.textContent=counter_
-  
+  	counterimage.textContent=counter_ + " of " + sizeListImgs ;
+	  	setTimeout(()=>{
+	  		goimg.classList.remove("scale")
+	  	},300)
 	}, false)
 // back click
 		backimg.addEventListener("click", (e)=>{
- 
+ 		 
   		
 		if (counter_ != 0){
 	 		counter_ -= 1;
-	 		counterimage.textContent=counter_
+	 		counterimage.textContent=counter_ + " of " + sizeListImgs;
 		 	imgitem[0].src = routeimg+"/"+listimgs[counter_];
+		 	e.target.classList.add("scale")
+
+			 setTimeout(()=>{
+	  			e.target.classList.remove("scale")
+	  		},300)
+
 		 }
+	
   	counterimage.style.backgroundColor = colors[counter_]
  }, false)
 
@@ -43,4 +54,5 @@ let listimgs = ["abra.jpg", "aerodactyl.jpg", "alakazam.jpg", "arbok.jpg",
 			    sizeListImgs = listimgs.length, 
 			    counter_ = 0;
 
+counterimage.textContent += " of "+ sizeListImgs
 goImage(goimg, backimg, listimgs, counter_, sizeListImgs, imgitem, routeimg, mainimg, counterimage);
